0959add47507db715c73bd3d9d0707c7777e4622
unknown <ya.ramdane01@gmail.com>
2025-12-13 22:03:58 +0100

    add AdminAuth

--- controllers/productController.js
+++ controllers/productController.js
@@ -70,7 +70,14 @@ const removeProduct = async (req,res) => {
 
 //function for single product info
 const singleProduct = async (req,res) => {
-    
+    try {
+        const { productId } = req.body
+        const product = await productModel.findById(productId)
+        res.json({success:true,product})
+    } catch (error) {
+        res.json({success:false, message:error.message})
+    }
 }
+
 
 export { listProduct,addProduct,removeProduct,singleProduct }
--- controllers/userController.js
+++ controllers/userController.js
@@ -77,7 +77,19 @@ const registerUser = async (req, res) => {
 }
 // routes pour admin login
 const adminLogin = async (req, res) => {
+    try {
+       const {email,password} = req.body 
 
+       if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+        const token = jwt.sign(email+password, process.env.JWT_SECRET);
+        res.json({success:true, token})
+       }else {
+        res.json({success:false, message:"invalid credentials"})
+       }
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+    }
 }
 
 
